532fb911c6f35c0b5c868d42b92209b636245e60
Allez <p_alexey@hotbox.ru>
2010-06-10 09:42:33 +0400

    update talents when it really needed

--- spec.lua
+++ spec.lua
@@ -1,6 +1,5 @@
 local elapsed = 0.5
-local addon = CreateFrame('Frame')
-local dataobj = LibStub:GetLibrary('LibDataBroker-1.1'):NewDataObject('Spec', {text = 'no spec', icon = "Interface\\Icons\\Spell_Shadow_SacrificialShield", iconCoords = {0.065, 0.935, 0.065, 0.935}})
+local dataobj = LibStub:GetLibrary('LibDataBroker-1.1'):NewDataObject('Spec', {type = "data source", text = 'no spec', icon = "Interface\\Icons\\Spell_Shadow_SacrificialShield", iconCoords = {0.065, 0.935, 0.065, 0.935}})
 
 function dataobj.OnLeave()
 	GameTooltip:SetClampedToScreen(true)
@@ -10,9 +9,8 @@ end
 function dataobj.OnEnter(self)
 	GameTooltip:SetOwner(self, 'ANCHOR_BOTTOMLEFT', 0, self:GetHeight())
 	GameTooltip:AddLine("Talents")
-	GameTooltip:AddLine("Hint: left-click to show talents pane.")
+	GameTooltip:AddLine("Hint: left-click to show talents frame.")
 	GameTooltip:AddLine("right-click to switch talents.")
-
 	GameTooltip:Show()
 end
 
@@ -28,21 +26,22 @@ function dataobj.OnClick(self, button)
 	end
 end
 
-addon:SetScript('OnUpdate', function(self, al)
-	elapsed = elapsed + al
-	if(elapsed > 0.5) then
-		local maxPoints, finalIcon, text = 0, "Interface\\Icons\\Spell_Shadow_SacrificialShield"
-		for tab = 1, 3 do
-			local _, icon, points = GetTalentTabInfo(tab,nil,nil,group)
-			if points > maxPoints then
-				maxPoints = points
-				finalIcon = icon
-			end
-			text = format("%s%.2i", text and text.."/" or "", points)
+local OnEvent = function(self, event, ...)
+	local maxPoints, finalIcon, text = 0, "Interface\\Icons\\Spell_Shadow_SacrificialShield"
+	for tab = 1, 3 do
+		local _, icon, points = GetTalentTabInfo(tab,nil,nil,group)
+		if points > maxPoints then
+			maxPoints = points
+			finalIcon = icon
 		end
-
-		dataobj.text = text
-		dataobj.icon = finalIcon
-		elapsed = 0
+		text = format("%s%.2i", text and text.."/" or "", points)
 	end
-end)
+	dataobj.text = text
+	dataobj.icon = finalIcon
+end
+
+local addon = CreateFrame('Frame')
+addon:SetScript('OnEvent', OnEvent)
+addon:RegisterEvent("PLAYER_ENTERING_WORLD")
+addon:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+addon:RegisterEvent("PLAYER_TALENT_UPDATE")
